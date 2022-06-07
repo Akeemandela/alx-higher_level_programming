f3fea38b1f8bf1ad50e86543f64408e52e1a0d3b
Akeemandela <easytwenty14@gmail.com>
2022-06-07 03:57:06 +0100

    updated data structures

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+
+    lenz = len(my_list)
+
+    if idx > lenz - 1:
+        return (my_list)
+
+    my_list[idx] = element
+
+    return (my_list)
