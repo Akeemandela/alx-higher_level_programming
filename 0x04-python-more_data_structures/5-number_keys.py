6a55e6caab11a432d51bc40bfc58449d7f52a655
Akeemandela <easytwenty14@gmail.com>
2022-06-09 03:26:13 +0100

    Update on more data structures

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-def number_keys(my_dict):
-    return len(my_dict)`#!/usr/bin/python3
-def number_keys(my_dict):
-        return len(my_dict)`#!/usr/bin/python3
-    def number_keys(my_dict):
-            return len(my_dict)`#!/usr/bin/python3
-        def number_keys(my_dict):
-                return len(my_dict)`#!/usr/bin/python3
-            def number_keys(my_dict):
-                    return len(my_dict)`#!/usr/bin/python3
-                def number_keys(my_dict):
-                        return len(my_dict)`#!/usr/bin/python3
-                    def number_keys(my_dict):
-                            return len(my_dict)`#!/usr/bin/python3
-                        def number_keys(my_dict):
-                                return len(my_dict)`#!/usr/bin/python3
-                            def number_keys(my_dict):
-                                    return len(my_dict)`
+def number_keys(a_dictionary):
+    num_key = 0
+    list_keys = list(a_dictionary.keys())
+
+    for i in list_keys:
+        num_key += 1
+
+    return (num_key)`
