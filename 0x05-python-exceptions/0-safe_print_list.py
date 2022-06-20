8acb2f6f6856c2f7a596c438fbc908c763186b6a
Akeemandela <easytwenty14@gmail.com>
2022-06-20 21:34:14 +0100

    The exceptions skilup

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(0, x):
+    for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print(my_list[i], end="")
             count += 1
-        except (ValueError, TypeError, IndexError):
+        except IndexError:
             break
     print()
-    return 
+    return count  
