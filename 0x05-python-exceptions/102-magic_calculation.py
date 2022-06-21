0d04d94cc3d56e1402ff5ca663b04464c2565735
Akeemandela <easytwenty14@gmail.com>
2022-06-21 02:08:22 -0700

    Updated exception files

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / i
-        except(ValueError, IndexError, TypeError):
+        except TypeError
             result = b + a
             break
     return result
