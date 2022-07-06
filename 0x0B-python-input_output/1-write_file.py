58015326ab449268e150f27a899fe212d5a3251d
Akeemandela <easytwenty14@gmail.com>
2022-07-06 05:04:34 +0100

    Reading and Writing files in python programming

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""defining write_file with two arguments"""
+"""Defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-    """reads filename with utf-8"""
+    """Write a string to a UTF8 text file.
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
 
