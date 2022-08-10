4579f33043d3dd65ddd08d3cd73698b39ed1ca4d
Akeemandela <easytwenty14@gmail.com>
2022-08-11 00:51:35 +0100

    sql project update

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- This script list all records of the table second_table of the MYSQL server.
-SELECT score, name, FROM second_table ORDER BY score DESC
+SELECT score, name, FROM second_table ORDER BY score DESC;
