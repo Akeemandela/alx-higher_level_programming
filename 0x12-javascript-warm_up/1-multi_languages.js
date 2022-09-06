b230decb1be00780ac604dac35758d6666ceecf9
Akeemandela <easytwenty14@gmail.com>
2022-09-06 15:06:10 +0100

    Javascript is amazing

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-#!/usr/bin/node
-let myWord = "C is fun";
-let myWord_2 = "Python is cool";
-let myWord_3 = "Javascript is amazing";
-||||||| d5cf0d3
-let myWord = "C is fun";
-let myWord_2 = "Python is cool";
-let myWord_3 = "Javascript is amazing";
-=======
 #!/usr/bin/node
 const myWord = 'C is fun';
 const myWordOne = 'Python is cool';
 const myWordTwo = 'Javascript is amazing';
->>>>>>> 4eea3d93d6af45a081541e734580d3f5ca5fb476
 console.log(myWord);
 console.log(myWordOne);
 console.log(myWordTwo);
