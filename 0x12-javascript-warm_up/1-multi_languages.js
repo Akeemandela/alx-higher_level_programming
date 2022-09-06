6bea0493df5e6851b6ef3e48514b6cbd396d5f77
Akeemandela <easytwenty14@gmail.com>
2022-09-06 05:47:08 +0100

    Javascript is amazing

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let myWord = "C is fun";
+let myWord_2 = "Python is cool";
+let myWord_3 = "Javascript is amazing";
+console.log(myWord);
+console.log(myWord_2);
+console.log(myWord_3);
