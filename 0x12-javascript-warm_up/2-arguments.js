07da4cd201db5d1b33f11ffae45bdc5e665e7069
Akeemandela <easytwenty14@gmail.com>
2022-09-06 14:17:17 +0100

    "Javascript is amazing"

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+import {argv} from 'node: process';
+if (argv == null){
+    console.log("No argument");
+}
+else if(argv == 1){
+    console.log("Argument found");
+}
+else(console.log("Argument found"))
