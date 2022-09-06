a95fbb1289bba034e2b778c4bffa0087e1d13565
Akeemandela <easytwenty14@gmail.com>
2022-09-06 15:20:46 +0100

    "Javascript is amazing"

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const process = require('process');
 let args = process.argv;
 args.forEach((val, index) => {
-    if (index == 0 || index == 1) {
+    if (index === 0 || index === 1) {
         console.log('No argument');
-    } else if (index == 2) {
+    } else if (index === 2) {
         console.log('Argument found');
     } else (console.log('Argument found'));
     console.log(`${index}: ${val}`);
