65d1e5e4c233cb75bd9f7f3608a6fd691450b530
Akeemandela <easytwenty14@gmail.com>
2022-09-06 13:52:18 +0100

    "Javascript is amazing"

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
-let myWord = "C is fun";
-let myWord_2 = "Python is cool";
-let myWord_3 = "Javascript is amazing";
+const myWord = 'C is fun';
+const myWord_2 = 'Python is cool';
+const myWord_3 = 'Javascript is amazing';
 console.log(myWord);
 console.log(myWord_2);
 console.log(myWord_3);
