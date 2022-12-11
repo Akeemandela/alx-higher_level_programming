afa61db6167991ce37aa7c8a69c08321ef29e7be
Akeemandela <easytwenty14@gmail.com>
2022-12-11 12:17:45 +0100

    Javascript web scraping implementation

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-
+/* A script that reads and prints the content of a file. */
 const fs = require('fs');
-const file = process.argv[2];
-
-fs.readFile(file, 'utf-8', function (err, data) {
-  if (err) {
-    console.log(err);
+const arg = process.argv[2];
+fs.readFile(arg, 'utf-8', function (error, content) {
+  if (error) {
+    console.log(error);
   } else {
-    console.log(data);
+    console.log(content);
   }
 });
+
